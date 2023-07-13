7e0e5b9fcdc03263937f9a2881c374a876210fe9
Rohit chaturvedi <rohitchaturvedi698@gmail.com>
2023-07-13 11:41:44 +0530

    44

---
+++ minimum_plateform.cpp
@@ -0,0 +1,19 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    // Write your code here.
+    sort(at,at+n);
+    sort(dt,dt+n);
+    int i=1, j = 0;
+    int ans = 1, plateform = 1;
+    while(i<n and j<n){
+        if(at[i]<=dt[j]){
+            i++;
+            plateform++;
+        }
+        else{
+            j++;
+            plateform--;
+        }
+        ans = max(ans,plateform);
+    }
+    return ans;
+}
